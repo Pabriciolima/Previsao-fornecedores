60849c4e032b0bfc015dc175ab2f195aa4dc03b5
pabricio lima <pabricio.dev.1@gmail.com>
2025-01-22 23:20:54 -0300

    Descrição das alterações feitas

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const fornecedores = [
     { fornecedor: 'Master', tipo: 'Teresina', prazo: '23 a 25 dias' },
     { fornecedor: 'Cummins U.P', tipo: 'São Luís - U.P.', prazo: '10 a 15 dias' },
     { fornecedor: 'Cummins', tipo: 'São Luís - Normal', prazo: '22 dias' },
-    { fornecedor: 'Eaton', tipo: 'U.P.', prazo: '15 dias' },
+    { fornecedor: 'Eaton U.P', tipo: 'U.P.', prazo: '15 dias' },
     { fornecedor: 'Eaton', tipo: 'Normal', prazo: '20 dias' },
     { fornecedor: 'Fras-le', tipo: 'Normal', prazo: '20 dias' },
-    { fornecedor: 'Pedido da Fábrica AEREO', tipo: 'Item que pode vir aéreo', prazo: '4 dias úteis - U.P.' },
-    { fornecedor: 'Pedido da Fábrica NÃO AEREO', tipo: 'Item que NÃO pode vir aéreo', prazo: '10 a 12 dias úteis - U.P.' },
+    { fornecedor: 'Pedido da Fábrica U.P AEREO', tipo: 'Item que pode vir aéreo', prazo: '4 dias úteis - U.P.' },
+    { fornecedor: 'Pedido da Fábrica U.P NÃO AEREO', tipo: 'Item que NÃO pode vir aéreo', prazo: '10 a 12 dias úteis - U.P.' },
     { fornecedor: 'Pedido da Fábrica', tipo: 'Normal', prazo: '15 a 17 dias úteis' },
     { fornecedor: 'Meritor', tipo: 'Normal', prazo: '20 dias' }
 ];
@@ -27,6 +27,9 @@ fornecedores.forEach(fornecedor => {
     item.addEventListener('click', () => {
         detalhesTexto.textContent = `Tipo de atendimento: ${fornecedor.tipo}\nPrazo para atender: ${fornecedor.prazo}`;
         detalhes.classList.add('show');
+        
+        // Faz o scroll até a seção de detalhes
+        detalhes.scrollIntoView({ behavior: 'smooth' });
     });
 
     fornecedoresList.appendChild(item);
